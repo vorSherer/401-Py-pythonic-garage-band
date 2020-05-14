c91f18c26781766a49dd5947b2ed6af664149fdd
vorSherer <tcpma2011@gmail.com>
2020-05-14 13:48:51 -0700

    Initial commit.

---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,26 @@
+class Musician:
+    def __init__ (self):
+        pass
+
+
+class Guitarist(Musician):
+    def __init__ (self):
+        pass
+
+
+class Bassist(Musician):
+    def __init__ (self):
+        pass
+
+
+class Drummer(Musician):
+    def __init__ (self):
+        pass
+
+
+class Band:
+    def __init__ (self):
+        self.name = name
+
+def test():
+    pass
---
+++ tests/test_pythonic.py
@@ -0,0 +1,4 @@
+from pythonic_garage_band/pythonic_garage_band import test
+
+def test_test():
+    pass
